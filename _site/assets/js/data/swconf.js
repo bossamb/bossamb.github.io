45decd7ee17b45eddf781e3e914f551061aaa6d0
yhjeong <yhjeong90@gmail.com>
2024-09-21 22:00:17 +0900

    npm check

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1726920324',resources: [
+    cacheName: 'chirpy-1726923487',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
